b9eec00be0120cd7d1100c0f241eb15eb0381975
Sergii Miroshnichenko <103072766+MiroshnichenkoSergii@users.noreply.github.com>
2022-07-30 13:38:56 +0300

    Add files via upload

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Sergii Miroshnichenko on 16.06.2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
