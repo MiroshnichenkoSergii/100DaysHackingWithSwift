fbe03bcf90d5803a5ede0d9585f214bc6f39f92d
Sergii Miroshnichenko <103072766+MiroshnichenkoSergii@users.noreply.github.com>
2022-07-30 13:39:50 +0300

    Add files via upload

---
+++ Project12b/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Sergii Miroshnichenko on 16.06.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
